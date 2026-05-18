476a1b6dd2b39138f02334b51fe858778132cf2b
Tserpilouski <terpilovskikirill@gmail.com>
2026-05-18 22:47:32 +0200

    feat: kiryl#EPAM-55 add input and change config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Input } from './shared/components/input/input';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'ec-root',
+  imports: [RouterOutlet, Input, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.email]),
+  });
+}
