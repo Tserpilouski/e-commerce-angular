789e881c1b3f30257f5bf989ce6c272187180df7
Tserpilouski <terpilovskikirill@gmail.com>
2026-07-05 19:53:32 +0200

    feat: kiryl#EPAM-16 add profile section with nested routes

---
+++ src/app/pages/profile/account/account.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ec-account',
+  imports: [],
+  templateUrl: './account.component.html',
+  styleUrl: './account.component.scss',
+})
+export class Account {}
---
+++ src/app/pages/profile/dashboard/components/address-card/address-card.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'ec-address-card',
+  imports: [MatIconModule],
+  templateUrl: './address-card.html',
+  styleUrl: './address-card.scss',
+})
+export class AddressCard {}
